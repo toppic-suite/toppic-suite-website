b4214d603063b48af25a442117d629e5eddb6366
Xiaowen <liuxiaowencs@gmail.com>
2017-09-10 14:04:05 -0400

    Working on saveinfo.php

--- saveinfo.php
+++ saveinfo.php
@@ -1,27 +1,48 @@
 <?php
- $first=$_REQUEST["first"];
- $last=$_REQUEST["last"];
- $email=$_REQUEST["email"];
- $org=$_REQUEST["org"];
 
- $bruce=fopen("register_info.txt","a");
- if(!$bruce)
- {
-    echo'file not exist!';
-    exit;
- }
- fwrite($bruce,$first);
- fwrite($bruce,"\t");
- fwrite($bruce,$last);
- fwrite($bruce,"\t");
- fwrite($bruce,$email);
- fwrite($bruce,"\t");
- fwrite($bruce,$org);
- fwrite($bruce,"\t");
- fwrite($bruce,$_SERVER['REMOTE_ADDR']);
- fwrite($bruce,"\t");
- fwrite($bruce,date('Y-m-d'));
- fwrite($bruce,"\n");
- fclose($bruce);
- header("Location: downloads.html");
+$first=$_REQUEST["first"];
+$last=$_REQUEST["last"];
+$email=$_REQUEST["email"];
+$org=$_REQUEST["org"];
+
+$reg_file=fopen("register_info.txt","a");
+if(!$reg_file)
+{
+	echo'file not exist!';
+	exit;
+}
+fwrite($reg_file,$first);
+fwrite($reg_file,"\t");
+fwrite($reg_file,$last);
+fwrite($reg_file,"\t");
+fwrite($reg_file,$email);
+fwrite($reg_file,"\t");
+fwrite($reg_file,$org);
+fwrite($reg_file,"\t");
+fwrite($reg_file,$_SERVER['REMOTE_ADDR']);
+fwrite($reg_file,"\t");
+fwrite($reg_file,date('Y-m-d'));
+fwrite($reg_file,"\n");
+fclose($reg_file);
+
+$file_name = 'toppic_windows.zip';
+$download_type=$_REQUEST["downloadtype"];
+echo 'result';
+echo $download_type;
+
+if ($download_type=="linux") {
+	$file_name = "toppic_linux.zip";
+}
+
+if (file_exists($file_name)) {
+	header('Content-Description: File Transfer');
+	header('Content-Type: application/octet-stream');
+	header('Content-Disposition: attachment; filename="'.basename($file_name).'"');
+	header('Expires: 0');
+	header('Cache-Control: must-revalidate');
+	header('Pragma: public');
+	header('Content-Length: ' . filesize($file_name));
+	readfile($file_name);
+exit;
+}
 ?>
