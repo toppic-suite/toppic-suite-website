adecee41a70126b9992c2bc3706171b5e42f90f9
Xiaowen <liuxiaowencs@gmail.com>
2017-11-15 10:19:04 -0500

    Working on saveinfo.php

--- saveinfo.php
+++ saveinfo.php
@@ -25,24 +25,45 @@ fwrite($reg_file,date('Y-m-d'));
 fwrite($reg_file,"\n");
 fclose($reg_file);
 
-$file_name = 'release_files/toppic_windows_1.1.0.zip';
+$file_name = 'release_files/toppic-windows-1.1.0.zip';
 $download_type=$_REQUEST["downloadtype"];
 echo 'result';
 echo $download_type;
 
 if ($download_type=="linux") {
-	$file_name = "release_files/toppic_linux_1.1.0.zip";
+	$file_name = "release_files/toppic-linux-1.1.0.zip";
 }
 
-if (file_exists($file_name)) {
-	header('Content-Description: File Transfer');
-	header('Content-Type: application/octet-stream');
-	header('Content-Disposition: attachment; filename="'.basename($file_name).'"');
-	header('Expires: 0');
-	header('Cache-Control: must-revalidate');
-	header('Pragma: public');
-	header('Content-Length: ' . filesize($file_name));
-	readfile($file_name);
-exit;
+if (headers_sent()) {
+  echo 'HTTP header already sent';
+} else {
+  if (!is_file($file_name)) {
+    header($_SERVER['SERVER_PROTOCOL'].' 404 Not Found');
+    echo 'File not found';
+  } else if (!is_readable($file_name)) {
+    header($_SERVER['SERVER_PROTOCOL'].' 403 Forbidden');
+    echo 'File not readable';
+  } else {
+    header($_SERVER['SERVER_PROTOCOL'].' 200 OK');
+    header("Content-Type: application/zip");
+    header("Content-Transfer-Encoding: Binary");
+    header("Content-Length: ".filesize($file_name));
+    header("Content-Disposition: attachment; filename=\"".basename($file_name)."\"");
+    readfile($file);
+    exit;
+  }
 }
+
+#if (file_exists($file_name)) {
+#  header('Content-Description: File Transfer');
+#  header('Content-Type: application/octet-stream');
+#  header('Content-Disposition: attachment; filename="'.basename($file_name).'"');
+#  header('Expires: 0');
+#  header('Cache-Control: must-revalidate');
+#  header('Pragma: public');
+#  header('Content-Length: ' . filesize($file_name));
+#  readfile($file_name);
+#  exit;
+#}
+#
 ?>
